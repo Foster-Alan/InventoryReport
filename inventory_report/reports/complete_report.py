cf67d503a5b61931b9e90984d33b496841b81548
Alan Foster <alan-af@outlook.com>
2023-05-08 15:48:49 -0300

    req 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,16 @@
+from .simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(data):
+        simple = SimpleReport.generate(data)
+        company = [i["nome_da_empresa"] for i in data]
+        company_item = Counter(company)
+        report = f"{simple}\nProdutos estocados por empresa:\n"
+        for i in set(company):
+            quantidade = company_item[i]
+            report += f"- {i}: {quantidade}\n"
+
+        return report
