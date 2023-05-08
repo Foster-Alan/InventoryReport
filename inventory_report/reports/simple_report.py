93646a9989ce1d4165b61b7d42375aebf808c175
Alan Foster <alan-af@outlook.com>
2023-05-08 15:32:19 -0300

    req 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,23 @@
+from collections import Counter
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(data):
+        old_date = min(i["data_de_fabricacao"] for i in data)
+        prox_validate = min(
+            (i["data_de_validade"] for i in data if i
+                ["data_de_validade"] >= datetime.now().strftime("%Y-%m-%d"))
+        )
+
+        company = [i["nome_da_empresa"] for i in data]
+        more_products = Counter(company).most_common(1)[0][0]
+
+        report = (
+            f"Data de fabricação mais antiga: {old_date}\n"
+            f"Data de validade mais próxima: {prox_validate}\n"
+            f"Empresa com mais produtos: {more_products}"
+        )
+
+        return report
